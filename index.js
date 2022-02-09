67371786544bba4c4d2c625a32bc7064e8f3821b
Brian Vu <brianvu7@gmail.com>
2022-02-09 09:44:58 -0600

    commit tree d8814a83a6d23153799afc8552efcf45a3b16248

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+const submitData = (name, email) => {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(res => {
+        if(res.ok){
+            return res.json();
+          }
+          else{
+            throw new Error(`${res.status}: ${res.statusText}`);
+        }
+    })
+    .then(data => {
+        console.log(data);
+        document.body.append(data.id);
+    })
+    .catch(err => {
+        document.body.append(err);
+    });
+}
